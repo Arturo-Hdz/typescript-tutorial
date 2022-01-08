eacd61b88c3829070e2d00c7f4654d837adedd53
baruch <lb@LBs-MacBook-Air.home>
2022-01-08 15:56:25 -0500

    rendering an HTML template  #17

--- public/app.js
+++ public/app.js
@@ -17,31 +17,7 @@ console.log(me);
 // ------------ interface ------------
 import { Invoice } from "./classes/Invoice.js";
 import { Payments } from "./classes/Payments.js";
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payments('luigi', 'plumbing work', 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
-// -------- interfaces with classes
-// instance
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-console.log(invOne, invTwo);
-// invOne.client = 'yoshi';
-// invTwo.amount = 900;
-//array
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-// readonly inv.client = 'somenthing';
-console.log(invoices);
-invoices.forEach(inv => {
-    //console.log(inv.client, inv.details, inv.amount, inv.format());
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { ListTemplate } from "./classes/ListTemplate.js";
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
 //console.log(form.children);
@@ -50,6 +26,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -59,6 +38,7 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payments(tofrom.value, details.value, amount.valueAsNumber);
     }
+    list.render(doc, type.value, 'end');
     console.log(doc);
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -30,40 +30,7 @@ console.log(me);
 import { Invoice } from "./classes/Invoice.js";
 import { Payments } from "./classes/Payments.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payments('luigi', 'plumbing work', 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-console.log(docs);
-
-// -------- interfaces with classes
-
-// instance
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-console.log(invOne, invTwo);
-
-// invOne.client = 'yoshi';
-// invTwo.amount = 900;
-
-//array
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-// readonly inv.client = 'somenthing';
-console.log(invoices);
-
-invoices.forEach(inv => {
-    //console.log(inv.client, inv.details, inv.amount, inv.format());
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { ListTemplate } from "./classes/ListTemplate.js";
 
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
@@ -75,6 +42,10 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -84,6 +55,8 @@ form.addEventListener('submit', (e: Event) => {
     } else {
         doc = new Payments(tofrom.value, details.value, amount.valueAsNumber)
     }
+
+    list.render(doc, type.value, 'end');
 
     console.log(doc);
 
