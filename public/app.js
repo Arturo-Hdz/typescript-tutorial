0eafb864a881cfe8ea2a9bfada9fb7ff2280ddb7
baruch <lb@LBs-MacBook-Air.home>
2022-01-08 01:17:40 -0500

    public, private &readonly  #13

--- public/app.js
+++ public/app.js
@@ -5,25 +5,40 @@
 // }
 //    console.log(anchor.href);
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    // constructor(c: string, d: string, a: number){
+    // this.client = c;
+    // this.details = d;
+    // this.amount = a;
+    // }
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
-        return `${this.client} owes $ ${this.amount} for $(this.details)`;
+        // this.client = 'somenthing else';
+        return `${this.client} owes $ ${this.amount} for ${this.details}`;
     }
 }
+// instance
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 console.log(invOne, invTwo);
-invOne.client = 'yoshi';
-invTwo.amount = 900;
+// invOne.client = 'yoshi';
+// invTwo.amount = 900;
 //array
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
+// readonly inv.client = 'somenthing';
 console.log(invoices);
+invoices.forEach(inv => {
+    //console.log(inv.client, inv.details, inv.amount, inv.format());
+    console.log(inv.client, inv.amount, inv.format());
+});
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
 //console.log(form.children);
