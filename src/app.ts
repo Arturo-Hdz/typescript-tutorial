9d64e29c2946a205dfbcebaaf2e4e3acb2c2faca
baruch <lb@LBs-MacBook-Air.home>
2022-01-08 16:51:38 -0500

    generics  #18

--- src/app.ts
+++ src/app.ts
@@ -68,3 +68,35 @@ form.addEventListener('submit', (e: Event) => {
     );
 })
 
+// Generics
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+console.log(docOne);
+
+// with interfaces
+interface Resource <T>{
+    uid: number;
+    resourceName: string;
+    //data: '?????';
+    // data: object;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: {name: 'Shaun'}
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll']
+}
+
+console.log(docThree, docFour);
